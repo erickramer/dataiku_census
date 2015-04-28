62b9cb3e634bc8e8bbc43ec51f7004cb576f3ae5
Eric Kramer <ericransomkramer@gmail.com>
2015-04-28 16:54:43 +0200

    converting X19 to numeric

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -7,18 +7,20 @@ convert_to_numeric = function(x) as.numeric(gsub("^ ", "", x))
 
 # loading the data from the file
 # and a bit of cleaning
+
 census_training = read_csv("./data/us_census_full/census_income_learn.csv",
                            col_names=F) %>%
   mutate(target=X42, age=X1) %>% # I'm guessing X1 is age
   mutate(X25=convert_to_numeric(X25)) %>%
   mutate(X6=convert_to_numeric(X6)) %>% 
+  mutate(X19=convert_to_numeric(X19)) %>%
   select(-X42, -X1)
 
 
 # now i'm looking at feature counts
 # making sure all levels have a reasonable frequency
 feature_counts = census_training %>%
-  select(-target, -age, -X25) %>% # remove continuous variables and target
+  select(-target, -age, -X25, -X6) %>% # remove continuous variables and target
   gather(variable, value) %>%
   group_by(variable, value) %>%
   summarize(n=n(),
@@ -27,6 +29,6 @@ feature_counts = census_training %>%
 # let's plot these frequencies to see what's going on
 feature_counts %>%
   ggvis(~f, fill = ~variable) %>%
-  layer_histograms()
+  layer_histograms(width=0.01)
 
 # we see that X5 
