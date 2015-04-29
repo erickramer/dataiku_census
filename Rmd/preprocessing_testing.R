94af6cb5cf8ebe30e7076d5d641459745e324177
Eric Kramer <ericransomkramer@gmail.com>
2015-04-29 20:37:16 +0200

    deleting preprocessing_testing script

--- Rmd/preprocessing_testing.R
+++
@@ -1,5 +0,0 @@
-library("dplyr") # used for  improved syntax
-library("data.table") # used for fast file reading
-library("tidyr") # used for fast data-munging
-library("ggvis") # used for plotting
-
