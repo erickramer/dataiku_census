e6704e75fef222eea9f44a95cf3ef3e5c2747eb4
Eric Kramer <ericransomkramer@gmail.com>
2015-04-29 20:36:37 +0200

    realized there's a meta data file...

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -6,99 +6,123 @@ library("ggvis") # used for plotting
 # loading the data from the file
 # and a bit of cleaning
 
-census_training = fread("./data/us_census_full/census_income_learn.csv") %>%
-  mutate(target=V42) %>%
-  mutate(id=paste("ID", 1:nrow(.), sep="")) %>%
-  select(-V42) %>%
-  as.tbl
+meta_data = read_tsv("./data/us_census_full/meta_data.txt") 
+
+census_training = fread("./data/us_census_full/census_income_learn.csv",
+                        colClasses=ifelse(meta_data$Class=="continuous", 
+                                          "numeric", 
+                                          "character"))
+
+census_testing = fread("./data/us_census_full/census_income_test.csv",
+                       colClasses=ifelse(meta_data$Class=="continuous", 
+                                         "numeric", 
+                                         "character")) 
+# reset data names
+setnames(census_training, colnames(census_training), meta_data$Variable)
+setnames(census_testing, colnames(census_testing), meta_data$Variable)
+
+# combine two sets together 
+# to ensure consistent preprocessing
+census = rbind(census_training %>% mutate(Cohort="Training"),
+               census_testing %>% mutate(Cohort="Testing")) %>%
+  mutate(id=paste0("ID", 1:nrow(.)))
 
 # quick look at the data
 str(census_training)
 
-# getting a little meta-info about the variables
-classes = data.frame(variable=colnames(census_training),
-                     class=sapply(census_training, class),
-                     stringsAsFactors=F)
 
 # i'm going to split the data into catagorical and continous
 # for the next couple cleaning steps
 
 ## CLEANING CATAGORICAL VARIABLES 
 
-continuous_vars = classes %>%
-  filter(class %in% c("integer", "numeric")) %>%
-  .$variable
+continuous_vars = meta_data %>%
+  filter(Class=="continuous") %>%
+  .$Variable
 
-catagorical_vars = classes %>%
-  filter(class=="character") %>%
-  filter(variable!="target") %>%
-  filter(variable!="id") %>%
-  .$variable
+categorical_vars = meta_data %>%
+  filter(Class=="categorical") %>%
+  filter(Variable!="target") %>%
+  .$Variable
 
 # i'm looking at feature counts for catagorical
-# making sure all levels have a reasonable frequency
+# making sure all entries have a reasonable frequency
+# these frequencies are based solely on the training data
 
-catagorical_counts = census_training %>%
-  select(one_of(catagorical_vars)) %>%
-  gather(variable, value) %>%
-  group_by(variable, value) %>%
+categorical_counts = census_training %>%
+  select(one_of(categorical_vars)) %>%
+  gather(Variable, Value) %>%
+  group_by(Variable, Value) %>%
   summarize(n=n()) %>%
   ungroup %>%
   mutate(f=n/nrow(census_training)) %>%
   ungroup
 
 # let's plot these frequencies to see what's going on
-catagorical_counts %>%
-  ggvis(~f, fill = ~variable) %>%
+categorical_counts %>%
+  as.data.frame %>%
+  ggvis(~f, fill =~ Variable) %>%
   layer_histograms(width=0.01)
 
-# we see that V35, V34 and V33 have a lot of rare catagories
-# these look like "country of birth" or something like that
-# V22-V23 also have a lot of rare catagories
-# I'm going to set all catagories with freq < 5% to "rare"
+# we see that the DetailedIndustry, StateOfResidency, Occupation all have 
+# a lot of rare varaibles
 # 
-# I'm going to also delete two columns (V15, V38) with a max frequency > 95%
+# I'm going to also delete two columns (UnemployReason, VeteranAdmin) with a max frequency > 95%
 # these columns have very little variation
 
-rare_catagories = catagorical_counts %>%
+rare_categories = categorical_counts %>%
   filter(f < 0.05)
 
-frequent_catagories = catagorical_counts %>%
-  group_by(variable) %>%
+frequent_categories = categorical_counts %>%
+  group_by(Variable) %>%
   summarize(f=max(f)) %>%
   filter(f > 0.95)
 
-catagorical_cleaned = census_training %>%
-  select(one_of(catagorical_vars), id) %>%
-  gather(variable, value, -id) %>%
-  anti_join(frequent_catagories) %>%
-  anti_join(rare_catagories) %>%
-  spread(variable, value, fill="rare")
+categorical_cleaned = census %>%
+  select(one_of(categorical_vars), id) %>%
+  gather(Variable, Value, -id) %>%
+  anti_join(frequent_categories) %>%
+  anti_join(rare_categories) %>%
+  spread(Variable, Value, fill="RARE")
 
 ## CLEANING CONTINUOUS VARIABLES 
 
-## looking at the number of distinct values in the continuous variables
-## didn't end up chaning anything...
+# looking at the number of distinct values in the continuous variables
+# and the number of non-zero entries
+# CapitalLosses has very few nonzero entries, but I'll leave it for now
 
-continous_counts = census_training %>%
-  select(one_of(continuous_vars), id) %>%
-  gather(variable, value, -id) %>%
+continuous_counts = census_training %>%
+  select(one_of(continuous_vars)) %>%
+  gather(variable, value) %>%
   group_by(variable) %>%
-  summarize(n=n_distinct(value))
+  summarize(n=n_distinct(value),
+            n_nonzero=sum(value!=0))
 
-continuous_cleaned = census_training %>%
-  select(one_of(continuous_vars), id)
+# I'm adding a few indicator variables here 
+# to see whether or not the person reported CapitalGains, Losses, etc
+continuous_cleaned = census %>%
+  select(one_of(continuous_vars), id) %>%
+  mutate(FiledCapitalGains=CapitalGains!=0) %>%
+  mutate(FiledCapitalLosses=CapitalLosses!=0) %>%
+  mutate(FiledCapitalGains=CapitalGains!=0) %>%
+  mutate(EarnedWages=WagePerHour!=0)
 
 ## CREATING FINAL DATASET
 
-census_training = census_training %>%
-  select(id, target) %>%
-  mutate(target=factor(target)) %>%
+census = census %>%
+  select(id, Target, Cohort) %>%
+  mutate(Target=factor(Target)) %>%
   inner_join(catagorical_cleaned) %>%
   inner_join(continuous_cleaned)
 
+census_training = census %>%
+  filter(Cohort=="Training")
+
+census_testing = census %>%
+  filter(Cohort=="Testing")
+
 save(census_training, file="./data/census_training.Rdata")
+save(census_testing, file="./data/census_testing.Rdata")
+
 save(rare_catagories, file="./data/rare_catagories.Rdata")
 save(frequent_catagories, file="./data/frequent_catagories.Rdata")
-save(catagorical_counts, file="./Rmd/data/catagorical_counts.Rdata")
-save(classes, file="./data/classes.Rdata")
