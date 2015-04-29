3ead49d914c2c59c72f4986d171f7a760637719c
Eric Kramer <ericransomkramer@gmail.com>
2015-04-29 19:01:09 +0200

    updates to ensemble training

--- models/random_forest.R
+++ models/random_forest.R
@@ -1,3 +1,10 @@
+### This script trains four models
+### First it trains an RF, SVM and Elastic net regression
+### using cross-validation to find optimal parameters
+###
+### Then, is uses the predictions from those models to build 
+### an ensemble model
+
 library("caret")
 library("randomForest")
 library("kernlab")
@@ -7,32 +14,55 @@ library("dplyr")
 library("glmnet")
 
 set.seed(1)
+registerDoParallel(15)
 
-registerDoParallel(10)
+## PREDICTION FUNCTION WRAPPERS
 
-# load training data
+get_model_predictions = function(newdata, rf, svm, net){
+  data.frame(rf=predict(rf, newdata=newdata, type="prob"),
+             svn=predict(svm, newdata=newdata, type="prob"),
+             net=predict(net, newx=newdata, s="lambda.min"))
+}
 
+get_ensemble_predictions = function(newdata, ensemble, ...){
+  z = get_model_predictions(...)
+  predict(ensemble, newx=as.matrix(z), s="lambda.min")
+}
+
+## GENERATE TRAINING SETS
+
+# load full data
 train_full = get(load("./data/census_training.Rdata")) %>%
-  mutate(target=factor(target))
+  mutate(target=factor(target)) %>%
+  as.data.frame %>%
+  as.tbl
 
+# first training set is used to train the RF, SVM and Elastic Net
 train1 = train_full %>%
-  sample_n(5000)
+  group_by(target) %>% 
+  sample_n(5000) %>% # downsampling for quick training
+  ungroup 
 
 x1 = train1 %>%
   select(-id, -target) %>%
-  model.matrix(~., data=.)
+  model.matrix(~.-1 , data=.) # create dummy variables
 
 y1 = train1$target
 
+# second training set is used to train the ensemble model
 train2 = train_full %>%
   anti_join(train1 %>% select(id)) %>%
-  sample_n(5000)
+  group_by(target) %>% 
+  sample_n(500) %>% # downsampling for quick training
+  ungroup 
 
 x2 = train2 %>%
   select(-id, -target) %>%
-  model.matrix(~., data=.)
+  model.matrix(~., data=.) # create dummy variables
 
 y2 = train2$target
+
+## TRAINING MODELS
 
 # create control object for caret models
 ctrl = trainControl(method="repeatedcv",
@@ -47,6 +77,12 @@ rf = train(x1,
            method="rf",
            trControl=ctrl,
            ntrees=1000)
+
+rf_importance = rf$finalModel %>%
+  importance %>%
+  as.data.frame %>%
+  mutate(variable=row.names(.)) %>%
+  arrange(-MeanDecreaseGini)
 
 # grid search for svm 
 svm = train(x1,
@@ -65,6 +101,12 @@ net = cv.glmnet(x1,
              family="binomial",
              parallel=T)
 
+net_coef = coef(net) %>%
+  as.matrix %>%
+  as.data.frame %>%
+  mutate(variable=row.names(.)) %>%
+  arrange(-abs(coef))
+
 # create new predictors for ensemble models
 
 z2 = data.frame(rf=predict(rf, newdata=x2, type="prob"),
@@ -75,4 +117,10 @@ ensemble = cv.glmnet(x2,
                      y2,
                      keep=T,
                      nfolds=10,
-                     alpha=0.1)
+                     alpha=0.1,
+                     lower.limit=0)
+
+ensemble_coef = coef(net) %>%
+  as.matrix %>%
+  as.data.frame %>%
+  mutate(model=row.names(.))
