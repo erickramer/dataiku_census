28200530eda210fff52cab84da437e9ce3391846
Eric Kramer <ericransomkramer@gmail.com>
2015-04-29 19:25:18 +0200

    preprocessing for testing dataset

---
+++ Rmd/preprocessing_testing.R
@@ -0,0 +1,5 @@
+library("dplyr") # used for  improved syntax
+library("data.table") # used for fast file reading
+library("tidyr") # used for fast data-munging
+library("ggvis") # used for plotting
+
